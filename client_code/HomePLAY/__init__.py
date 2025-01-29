9cbf495ca6630b2da88dbfb3d25f84377d72e6dd
grippe@blue-way.net <grippe@blue-way.net>
2025-01-29 13:58:10 +0000

    Created ServerModule1; Edited settings, custom package dependencies and HomePLAY

--- client_code/HomePLAY/__init__.py
+++ client_code/HomePLAY/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import HomePLAYTemplate
 from anvil import *
+import anvil.server
 
 
 class HomePLAY(HomePLAYTemplate):
@@ -8,3 +9,9 @@ class HomePLAY(HomePLAYTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def join_new_game_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    games_info = anvil.server.call('get_latest_game')
+    print(games_info)
+    pass
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,21 @@
+import anvil.server
+import pymysql
+
+def connect():
+  connection = pymysql.connect(host='w014f358.kasserver.com',
+                               port=3306,
+                               user='d0429633',
+                               password = '7Pzfz6zBCSdNLRtbvXDV',
+                               database='d0429633',
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+  return connection
+
+@anvil.server.callable
+def get_latest_game(name):
+  conn = connect()
+  with conn.cursor() as cur:
+    cur.execute("SELECT * FROM games_info")
+    return cur.fetchall()
+
+  
