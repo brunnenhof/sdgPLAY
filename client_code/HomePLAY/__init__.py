2989931ca8bbb242c0f606fbe81fd9c4923d7fa4
grippe@blue-way.net <grippe@blue-way.net>
2025-01-30 09:52:29 +0000

    Edited HomePLAY

--- client_code/HomePLAY/__init__.py
+++ client_code/HomePLAY/__init__.py
@@ -27,6 +27,7 @@ class HomePLAY(HomePLAYTemplate):
     return game_id, npbhp
 
   def btn_gameID_ok_click(self, **event_args):
+    global game_id_entered
     game_id_entered = self.check_gameID.text
     games_info = anvil.server.call('get_entered_game', game_id_entered)
     # TODO
@@ -35,14 +36,17 @@ class HomePLAY(HomePLAYTemplate):
     # handle both if not
     game_id = games_info['game_id']
     roles = anvil.server.call('get_roles', game_id_entered)
-    print (roles)
+    self.start_up.visible = False
+    self.check_gameID_card.visible = False
+    #print (roles)
     # get fill_roles .....
-    self.radio_button_us.enabled = False
+    # self.radio_button_us.enabled = False
     
     """This method is called when the button is clicked"""
     self.take_role_card.visible = True
 
-  def radio_button_cn_clicked(self, game_id_entered, **event_args):
+  def radio_button_cn_clicked(self, **event_args):
+    global game_id_entered
     """This method is called when this radio button is selected"""
-    ministries = anvil.server.call('get_roles_for_a_region', (game_id_entered, 'cn'))
+    ministries = anvil.server.call('get_roles_for_a_region', game_id_entered, 'cn')
     print (ministries)
