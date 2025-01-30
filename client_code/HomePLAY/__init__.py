6786b40c50d047d2c68ce75d54cf1d29d20a30f3
grippe@blue-way.net <grippe@blue-way.net>
2025-01-30 13:18:24 +0000

    Edited HomePLAY

--- client_code/HomePLAY/__init__.py
+++ client_code/HomePLAY/__init__.py
@@ -45,8 +45,100 @@ class HomePLAY(HomePLAYTemplate):
     """This method is called when the button is clicked"""
     self.take_role_card.visible = True
 
-  def radio_button_cn_clicked(self, **event_args):
+  def radio_button_af_clicked(self, **event_args):
     global game_id_entered
+    print ('in af btn ' + game_id_entered)
     """This method is called when this radio button is selected"""
     ministries = anvil.server.call('get_roles_for_a_region', game_id_entered, 'cn')
+    for key in ministries:
+      if key == 'empowerment':
+        self.radio_button_emp.visible = ministries[key]
+      elif key == 'poverty':
+        self.radio_button_pov.visible = ministries[key]
+      elif key == 'inequality':
+        self.radio_button_ineq.visible = ministries[key]
+      elif key == 'food':
+        self.radio_button_ineq.visible = ministries[key]
+      elif key == 'energy':
+        self.radio_button_ineq.visible = ministries[key]
+      elif key == 'future':
+        self.radio_button_ineq.visible = ministries[key]
+
+  
+  def radio_button_cn_clicked(self, **event_args):
+    global game_id_entered
+    print ('in cn btn ' + game_id_entered)
+    """This method is called when this radio button is selected"""
+    ministries = anvil.server.call('get_roles_for_a_region', game_id_entered, 'cn')
+    for key in ministries:
+      if key == 'empowerment':
+        if ministries[key] == 0:
+          self.radio_button_emp.visible = False
+        else:
+          self.radio_button_emp.visible = True
+      elif key == 'poverty':
+        if ministries[key] == 0:
+          self.radio_button_pov.visible = False
+        else:
+          self.radio_button_pov.visible = True
+      elif key == 'inequality':
+        if ministries[key] == 0:
+          self.radio_button_ineq.visible = False
+        else:
+          self.radio_button_ineq.visible = True
+      elif key == 'food':
+        if ministries[key] == 0:
+          self.radio_button_ineq.visible = False
+        else:
+          self.radio_button_ineq.visible = True
+      elif key == 'energy':
+        if ministries[key] == 0:
+          self.radio_button_ineq.visible = False
+        else:
+          self.radio_button_ineq.visible = True
+      elif key == 'future':
+        if ministries[key] == 0:
+          self.radio_button_ineq.visible = False
+        else:
+          self.radio_button_ineq.visible = True
     print (ministries)
+
+  def radio_button_me_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in me btn ' + game_id_entered)
+    pass
+
+  def radio_button_us_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in us btn ' + game_id_entered)
+    pass
+
+  def radio_button_sa_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in sa btn ' + game_id_entered)
+    pass
+
+  def radio_button_la_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in la btn ' + game_id_entered)
+    pass
+
+  def radio_button_pa_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in pa btn ' + game_id_entered)
+    pass
+
+  def radio_button_ec_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in ec btn ' + game_id_entered)
+    pass
+
+  def radio_button_eu_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in eu btn ' + game_id_entered)
+    pass
+
+  def radio_button_se_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    print ('in se btn ' + game_id_entered)
+    pass
