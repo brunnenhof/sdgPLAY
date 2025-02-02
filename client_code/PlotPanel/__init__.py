22ea21c28dd6e0ad4986b24dd2172d073fb5c3d8
grippe@blue-way.net <grippe@blue-way.net>
2025-02-02 18:26:43 +0000

    Created PlotPanel; Edited HomePLAY

---
+++ client_code/PlotPanel/__init__.py
@@ -0,0 +1,12 @@
+from ._anvil_designer import PlotPanelTemplate
+from anvil import *
+import plotly.graph_objects as go
+import anvil.server
+
+
+class PlotPanel(PlotPanelTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
