aa484e9cdc548f01814abb8f839c4df5cfc60173
grippe@blue-way.net <grippe@blue-way.net>
2025-02-03 14:37:02 +0000

    Edited HomePLAY

--- client_code/HomePLAY/__init__.py
+++ client_code/HomePLAY/__init__.py
@@ -3,12 +3,10 @@ from anvil import *
 import plotly.graph_objects as go
 import anvil.server
 
-
 class HomePLAY(HomePLAYTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
 
   def join_new_game_click(self, **event_args):
